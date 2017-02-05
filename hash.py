f98527c6865a2903ab2dcac37d7bab9c9b70b324
Gil <gilpaly@gmail.com>
2017-02-05 11:59:50 -0800

    update

--- hash.py
+++ hash.py
@@ -1,18 +1,19 @@
-import md5 
+import md5
+import sys 
 
 def main():
-    filename = raw_input()    
+    if len(sys.argv) < 2:
+        print "%s [filename]" % sys.argv[0]
+        exit()
+    filename = sys.argv[1]
+    file_object = open(filename, 'rb')   
+    data = file_object.read()    
+
     m = md5.new()
-    m.update(filename)
-    print m.hexdigest()    
-        
-                 
-
-
-
-
-
-
+    m.update(data)
+    print m.hexdigest()
+    
+    file_object.close()    
 
 if __name__ == "__main__":
     main()
